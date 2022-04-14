f14dcfe56ad200f24d103e8ac56f1359af84fc36
ici806 <ici806@DESKTOP-DF4NS8A>
2022-04-14 11:16:28 +0900

    AOP 연습-xml

---
+++ src/main/java/com/javatest/ex/LogAop.java
@@ -0,0 +1,25 @@
+package com.javatest.ex;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class LogAop {
+
+	public Object loggerAop(ProceedingJoinPoint joinPoint) throws Throwable {
+		
+		String signatureStr = joinPoint.getSignature().toShortString(); // 타겟 메서드의 signature 정보
+		System.out.println(signatureStr + "메서드가 시작되었습니다.");
+		long st = System.currentTimeMillis(); // 메서드 호출 전 현재 시간을 long형 변수에 저장
+		
+		try {
+			Object obj = joinPoint.proceed(); // 타겟의 메서드(핵심 기능)를 호출
+			return obj;
+		}finally {
+			long et = System.currentTimeMillis(); // 메서드 호출 후 현재 시간
+			System.out.println(signatureStr + "메서드가 종료되었습니다.");
+			System.out.println(signatureStr + "메서드의 작업 실행시간 : " + (et - st) + "ms" );
+		}
+	}
+	
+	
+	
+}
